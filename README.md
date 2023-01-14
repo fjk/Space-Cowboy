c0d87eedaca2b623a32576540f77e1c505362d51
Frank Kropp <frankkropp@icloud.com>
2023-01-15 00:36:02 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+
+# NAME
+
+Space::Cowboy - It's new $module
+
+# SYNOPSIS
+
+    use Space::Cowboy;
+
+# DESCRIPTION
+
+Space::Cowboy is ...
+
+# LICENSE
+
+Copyright (C) Frank Kropp.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Frank Kropp <frankkropp@icloud.com>
---
+++ lib/Space/Cowboy.pm
@@ -0,0 +1,39 @@
+package Space::Cowboy;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Space::Cowboy - It's new $module
+
+=head1 SYNOPSIS
+
+    use Space::Cowboy;
+
+=head1 DESCRIPTION
+
+Space::Cowboy is ...
+
+=head1 LICENSE
+
+Copyright (C) Frank Kropp.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Frank Kropp E<lt>frankkropp@icloud.comE<gt>
+
+=cut
+
